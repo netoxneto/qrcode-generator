986e0be1c8782cbb883d6c9ada45e5e7c9ffb4d3
netoxneto <netoxneto@hotmail.com>
2023-07-08 20:35:41 -0300

    modify

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
 const textInput = document.getElementById('input');
 const btnGenerate = document.getElementById('btn-generate');
 const viewQR = document.getElementById('qrcode');
-const viewDownload = document.getElementById('download');
+// const viewDownload = document.getElementById('download');
 
 textInput.focus();
 
 // Cria Tag 'a' Download
-let downloadButton = document.createElement('a');
+// let downloadButton = document.createElement('a');
 
 // Atribui as classes
-downloadButton.classList.add('btn')
-downloadButton.classList.add('btn-success')
-downloadButton.setAttribute('id', 'downloadButton')
+// downloadButton.classList.add('btn')
+// downloadButton.classList.add('btn-success')
+// downloadButton.setAttribute('id', 'downloadButton')
 
 // Atribui Valor
-let valueLink = document.createTextNode("Download");
-downloadButton.appendChild(valueLink)
+// let valueLink = document.createTextNode("Download");
+// downloadButton.appendChild(valueLink)
 
-btnGenerate.addEventListener('click', () => {
+btnGenerate.addEventListener('click', async () => {
 
     viewQR.innerHTML = '';
-    viewDownload.innerHTML = '';
 
     if (!textInput.value) {
         alert("Insira um valor!");
@@ -38,14 +37,18 @@ btnGenerate.addEventListener('click', () => {
         correctLevel: QRCode.CorrectLevel.H
     });
 
-    qrcode.makeCode(textInput.value);
+    await qrcode.makeCode(textInput.value);
 
+    let test = viewQR.children[1];
+    console.log(test.src)
     // Coloca na DOM
-    viewDownload.appendChild(downloadButton)
+    // viewDownload.appendChild(downloadButton)
 })
 
-let buttonDownload = document.getElementById('downloadButton');
+// let buttonDownload = document.getElementById('downloadButton');
 
-buttonDownload.addEventListener('click', ()=>{
-    alert('Funcionou')
-})
+// buttonDownload.addEventListener('click', ()=>{
+//     let test = viewQR.children[1]
+//     buttonDownload.setAttribute('href', test.src)
+//     console.log(test.src)
+// })

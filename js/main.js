f93b7525c28706cb1c1891bbf4afac639154b7bf
netoxneto <netoxneto@hotmail.com>
2023-07-08 22:06:20 -0300

    Modify Download

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 const textInput = document.getElementById('input');
 const btnGenerate = document.getElementById('btn-generate');
 const viewQR = document.getElementById('qrcode');
-// const viewDownload = document.getElementById('download');
+const viewDownload = document.getElementById('download');
 
 textInput.focus();
 
-// Cria Tag 'a' Download
-// let downloadButton = document.createElement('a');
-
-// Atribui as classes
-// downloadButton.classList.add('btn')
-// downloadButton.classList.add('btn-success')
-// downloadButton.setAttribute('id', 'downloadButton')
-
-// Atribui Valor
-// let valueLink = document.createTextNode("Download");
-// downloadButton.appendChild(valueLink)
-
-btnGenerate.addEventListener('click', async () => {
-
+function createQR() {
     viewQR.innerHTML = '';
 
     if (!textInput.value) {
@@ -30,25 +17,30 @@ btnGenerate.addEventListener('click', async () => {
     let qrcode = new QRCode(viewQR, {
         width: 180,
         height: 180,
-        // colorDark: "#ffffff",
-        // colorLight: "#2b2a2a"
         colorDark: "#000000",
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H
     });
 
-    await qrcode.makeCode(textInput.value);
+    qrcode.makeCode(textInput.value);
+}
 
-    let test = viewQR.children[1];
-    console.log(test.src)
-    // Coloca na DOM
-    // viewDownload.appendChild(downloadButton)
-})
+function createButton() {
+    let downloadButton = document.createElement('a');
+    downloadButton.classList.add('btn');
+    downloadButton.classList.add('btn-success');
+    downloadButton.setAttribute('id', 'downloadButton');
+    downloadButton.textContent = "Download";
+    return downloadButton;
+}
 
-// let buttonDownload = document.getElementById('downloadButton');
-
-// buttonDownload.addEventListener('click', ()=>{
-//     let test = viewQR.children[1]
-//     buttonDownload.setAttribute('href', test.src)
-//     console.log(test.src)
-// })
+btnGenerate.addEventListener('click', () => {
+    createQR();
+    viewDownload.innerHTML = '';
+    viewDownload.appendChild(createButton());
+    setTimeout(() => {
+        let downloadButton = document.getElementById('downloadButton');
+        downloadButton.setAttribute('href', viewQR.querySelector('img').src);
+        downloadButton.setAttribute('download', 'qrcode.png');
+    }, 1);
+});
